5cbd7307e927548a8d8f7ae395c1a044518bc173
Liuwa113 <no-rival@foxmail.com>
2022-10-21 20:32:01 +0800

    暴露接口pic

--- router/index.js
+++ router/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 let router = express.Router();
 let render = require('../get');
-router.get('/animePicture', async (req, res) => {
+router.get('/pic', async (req, res) => {
   const count = req?.query?.count || 6;
   res.send(await render(count));
 });
